b4606a6a41556f10b48e10f16cf4979f59440b13
Anand Prabhu <97842562+Anandprabhu530@users.noreply.github.com>
2024-05-22 09:03:35 +0000

    Changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <div className="lg:pb-10 lg:pt-4 py-6 px-4 dark:bg-slate-800 dark:text-white font-poppins lg:font-bold font-semibold flex flex-col items-center">
       <div className="font-poppins font-bold text-3xl py-10">Project Work</div>
       <div className="lg:grid lg:grid-cols-3 md:grid-cols-2 grid lg:w-[65%] sm:lg:w-[85%] mx-auto gap-10">
-        {data.map((solo, ind) => (8
+        {data.map((solo, ind) => (
           <Display_Project key={ind} {...solo} />
         ))}
       </div>
